77c1488b55e7bc031c76be696aa1cca6871f6a2e
Rics <danil.klementev.29@gmail.com>
2025-03-28 13:47:22 +0300

    feat: add logo link to main page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,11 @@
 import { Button } from "antd";
+import Link from "next/link";
 
 const Header = () => {
     return (
         <header className="sticky top-0 bg-white shadow-md z-50">
             <div className="container mx-auto flex items-center justify-between py-4 px-6">
-                <div className="text-xl font-bold">Some company</div>
+                <Link href="/" className="text-xl font-bold">Some company</Link>
                 <Button type="primary" className="!px-12" href="/contacts">Contact us</Button>
             </div>
         </header>
